a80667daaabf1dd7c9dabd6662ee80c4ac688b4b
Preludio <123329775+SeuAilton@users.noreply.github.com>
2025-10-21 10:00:56 -0300

    resolution exercise 3 from testing_and_debugging

--- learn_to_Code_in_ Python/05 - testing_and_debugging/003/main.py
+++ learn_to_Code_in_ Python/05 - testing_and_debugging/003/main.py
@@ -1,2 +1,6 @@
 def take_magic_damage(health, resist, amp, spell_power):
-    pass
+    total_maximum_damage = spell_power * amp
+    actual_damage = total_maximum_damage - resist
+    new_health = health - actual_damage
+    return new_health
+
